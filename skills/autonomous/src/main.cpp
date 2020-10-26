2da6084b2141d6f046b713255bb11e1bae324997
TheCodingGuy15 <replytoyashn@gmail.com>
2020-10-26 19:25:10 +0000

    Added Analysis system

--- skills/autonomous/src/main.cpp
+++ skills/autonomous/src/main.cpp
@@ -174,8 +174,18 @@
 //Motor 3 and 4 are the LHS
 
 #include "vex.h"
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include<sstream>
 
 using namespace vex;
+
+//Variable Declaration
+bool leftSideSpin;
+bool rightSideSpin;
+std::ofstream ofs;
 
 // MARK: Adding a library for ease-of-use
 
@@ -244,30 +254,215 @@ void rollers(int speed){
 }
 
 
+//MARK: The Analysis system
+
+bool areSpinning(){ //Chacks if the system is spinning or not
+
+  bool isWorking;
+
+  if(Motor1.isSpinning() && Motor2.isSpinning()){
+
+    if(Motor3.isSpinning() && Motor4.isSpinning()){
+
+      rightSideSpin = true;
+
+    }
+
+    leftSideSpin = true;
+    isWorking = true;
+
+  } else if(Motor3.isSpinning() && Motor4.isSpinning()){
+
+    rightSideSpin = true;
+    isWorking = true;
+
+  } else {
+    isWorking = false;
+  }
+
+
+  //helps turn the boolean into a on/off like system that can help determine the state of motors at any given time
+
+  return isWorking; 
+  
+}
+
+
+class movement {
+  public:
+
+    std::stringstream battery;
+    std::stringstream batteryChange;
+    std::stringstream timeTaken;
+    std::stringstream motor1Torque;
+    std::stringstream motor2Torque;
+    std::stringstream motor3Torque;
+    std::stringstream motor4Torque;
+    std::stringstream temperatureChange;
+
+  /*A movement is the method that the system will store the data in*/
+
+};
+
+int calculateAverage(){
+  int total = Motor1.temperature(percent) + Motor2.temperature(percent) + Motor3.temperature(percent) + Motor4.temperature(percent);
+  int average = total / 4;
+
+  return average;
+}
+
+int analyseActions(){
+
+  //Calculate the data
+
+  /* Put into the SD Card
+  Brain.SDcard.savefile("analyse.h", data, sizeof(data));
+  */
+
+  int battery = Brain.Battery.voltage();
+  int batteryChange = 0;
+  int timeTaken = 0;
+  int motor1Torque = 0;
+  int motor2Torque = 0;
+  int motor3Torque = 0;
+  int motor4Torque = 0;
+  int startingTemperature = calculateAverage();
+  int temperatureChange = 0;
+
+  if(areSpinning()){
+
+    if(rightSideSpin && leftSideSpin !=true){
+      Brain.Timer.reset();
+      while(Motor3.isDone() != true){
+        motor3Torque = Motor3.torque();
+        motor4Torque = Motor4.torque();
+      }
+      batteryChange = battery - Brain.Battery.current();
+      timeTaken = Brain.Timer.value();
+      int32_t finalTemp = calculateAverage();
+      temperatureChange = startingTemperature - finalTemp;
+      
+
+    } else if(leftSideSpin &&  rightSideSpin !=true){
+      Brain.Timer.reset();
+      while(Motor2.isDone() != true){
+        motor1Torque = Motor1.torque();
+        motor2Torque = Motor2.torque();
+      }
+      batteryChange = battery - Brain.Battery.current();
+      timeTaken = Brain.Timer.value();
+      int32_t finalTemp = calculateAverage();
+      temperatureChange = startingTemperature - finalTemp;
+      
+
+    } else if(leftSideSpin && rightSideSpin){
+      Brain.Timer.reset();
+      while(Motor2.isDone() != true){
+        motor1Torque = Motor1.torque();
+        motor2Torque = Motor2.torque();
+      }
+
+      batteryChange = battery - Brain.Battery.current();
+      timeTaken = Brain.Timer.value();
+      int32_t finalTemp = calculateAverage();
+      temperatureChange = startingTemperature - finalTemp;
+
+    }
+
+    movement newMovement;
+    newMovement.battery << battery;
+    newMovement.batteryChange << batteryChange;
+    newMovement.timeTaken << timeTaken; 
+    newMovement.motor1Torque << motor1Torque;
+    newMovement.motor2Torque << motor2Torque;
+    newMovement.motor3Torque << motor3Torque;
+    newMovement.motor4Torque << motor4Torque;
+    newMovement.temperatureChange << temperatureChange;
+
+    
+
+    if(Brain.SDcard.isInserted()){
+
+      std::string battery1;
+      std::string batteryChange1;
+      std::string timeTaken1;
+      std::string motor1Torque1;
+      std::string motor2Torque1;
+      std::string motor3Torque1;
+      std::string motor4Torque1;
+      std::string temperatureChange1;
 
 
 
+      ofs.open("analysis.txt", std::ofstream::out); //Making the filename
+
+      //Adding the stringstream into a string
+
+      newMovement.battery >> battery1;
+      newMovement.batteryChange >> batteryChange1;
+      newMovement.timeTaken >> timeTaken1;
+      newMovement.motor1Torque >> motor1Torque1;
+      newMovement.motor2Torque >> motor2Torque1;
+      newMovement.motor3Torque >> motor3Torque1;
+      newMovement.motor4Torque >> motor4Torque1;
+      newMovement.temperatureChange >> temperatureChange1;
+
+      ofs << battery1;
+      ofs << batteryChange1;
+      ofs << timeTaken1;
+      ofs << motor1Torque1;
+      ofs << motor2Torque1;
+      ofs << motor3Torque1;
+      ofs << motor4Torque1;
+      ofs << temperatureChange1;
+
+      ofs.close();
+      
+
+      Brain.Screen.printAt(10, 40, "done");
+
+    } else {
+
+      Brain.Screen.printAt(10, 40, "No SD Card");
+
+    }
+
+    //Upload the data onto the sd card.
+
+  }
 
 
+  return 0;
+}
 
 
+int analyse(){
 
+  //Initialising the task3
 
+  while(true){
+
+    analyseActions();
+
+  }
+  
+  return 0;
+}
 
 
 
 int main() {
+
   // Initializing Robot Configuration. DO NOT REMOVE!
+
+  task analysis = task(analyse); //Using dual tasks for multitasking
+  
   vexcodeInit();
   wait(200, msec);
 
   while(Inertial20.isCalibrating()) {
     wait(5, msec);
   }
-
-  
-  
-  
 
   
 }
